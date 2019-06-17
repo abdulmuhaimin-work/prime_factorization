42c89cfe12b9e9c88e20096c803c2b82c2fa99b7
Abdul Muhaimin <abdulmuhaimin.work@gmail.com>
2019-06-17 14:06:49 +0800

    Initial commit

---
+++ lib/prime_factorization.ex
@@ -0,0 +1,18 @@
+defmodule PrimeFactorization do
+  @moduledoc """
+  Documentation for PrimeFactorization.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PrimeFactorization.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/prime_factorization_test.exs
@@ -0,0 +1,8 @@
+defmodule PrimeFactorizationTest do
+  use ExUnit.Case
+  doctest PrimeFactorization
+
+  test "greets the world" do
+    assert PrimeFactorization.hello() == :world
+  end
+end
