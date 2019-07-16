16f71fc9b371978949bad6deb3e77e31d2b8cf6d
Abdul Muhaimin <abdulmuhaimin.work@gmail.com>
2019-07-16 22:42:53 +0800

    first revision algorithm completed
    completed algorithm that work within a reasonable range. Not tested for performance yet

---
+++ .elixir_ls/build/test/lib/prime_factorization/ebin/prime_factorization.app
@@ -0,0 +1,6 @@
+{application,prime_factorization,
+             [{applications,[kernel,stdlib,elixir,logger]},
+              {description,"prime_factorization"},
+              {modules,['Elixir.PrimeFactorization','Elixir.Primes']},
+              {registered,[]},
+              {vsn,"0.1.0"}]}.
--- lib/prime_factorization.ex
+++ lib/prime_factorization.ex
@@ -12,7 +12,50 @@ defmodule PrimeFactorization do
       :world
 
   """
-  def hello do
-    :world
+
+  import Primes
+
+
+  def prime_numbers_factors(anything) do
+    nombor = case parse_input(anything) do
+      {:ok, parsed_number} -> parsed_number
+      {:error, err_message} -> IO.puts(err_message)
+    end
+
+    cond do
+      is_integer(nombor) -> division_check(nombor)
+      true -> IO.puts("please try again")
+    end
   end
+
+   def parse_input(input) do
+     cond do
+       is_integer(input) and input > largest_prime()*largest_prime() -> {:error, "number is too large. It only accept number below #{largest_prime()*largest_prime()}"}
+       is_integer(input) and input < 1 -> {:error, "number less than 1 is not acceptable"}
+       is_integer(input) -> {:ok, input}
+       true -> {:error, "not an integer"}
+     end
+   end
+
+  def division_check(number) do
+    division_check(number, 0, [])
+  end
+
+  def division_check(1, _index, factor) do
+    List.to_tuple(factor)
+  end
+
+  def division_check(number, index, factor) do
+    divident = elem(prime_numbers(), index)
+    square_root = :math.sqrt(number)
+    if divident > square_root do
+      division_check(1, 0, [number | factor])
+    else
+      case rem(number,divident) do
+        0 -> division_check(div(number,divident), 0, [divident | factor] )
+        _ -> division_check(number, index+1, factor)
+      end
+    end
+  end
+
 end
